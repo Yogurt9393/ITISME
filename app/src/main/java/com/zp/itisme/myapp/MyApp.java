7a7b0198780999f64e257fb8d8c6dcee0af603a2
Yogurt9393 <272230010@qq.com>
2018-09-11 18:29:02 +0800

    1

--- app/src/main/java/com/zp/itisme/myapp/MyApp.java
+++ app/src/main/java/com/zp/itisme/myapp/MyApp.java
@@ -1,5 +1,7 @@
 package com.zp.itisme.myapp;
 
+import android.app.Application;
+
 /**
  * Created by Android Stidio.
  *
@@ -7,5 +9,11 @@ package com.zp.itisme.myapp;
  * @email try.zp.catch@gmail.com
  * @date 2018/9/11
  */
-public class MyApp {
+public class MyApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
 }
