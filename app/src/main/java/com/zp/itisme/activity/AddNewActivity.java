6257ca4573d7de06b9e9629da914998d80b1db60
Yogurt9393 <272230010@qq.com>
2018-09-12 16:01:34 +0800

    1

--- app/src/main/java/com/zp/itisme/activity/AddNewActivity.java
+++ app/src/main/java/com/zp/itisme/activity/AddNewActivity.java
@@ -1,11 +1,17 @@
 package com.zp.itisme.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.zp.itisme.R;
 
-public class AddNewActivity extends BaseActivity {
+public class AddNewActivity extends BaseActivity implements View.OnClickListener {
+
+    private TextView tv_cancel;
+    private TextView tv_sure;
+    private EditText et_detail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +21,37 @@ public class AddNewActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        tv_cancel = (TextView) findViewById(R.id.tv_cancel);
+        tv_sure = (TextView) findViewById(R.id.tv_sure);
+        et_detail = (EditText) findViewById(R.id.et_detail);
+
 
     }
 
     @Override
     protected void setListener() {
+        tv_cancel.setOnClickListener(this);
+        tv_sure.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_cancel:
+                toCancel();
+                break;
+            case R.id.tv_sure:
+                toSubmit();
+                break;
+        }
+    }
+
+    private void toSubmit() {
 
     }
+
+    private void toCancel() {
+        AddNewActivity.this.finish();
+    }
+
 }
